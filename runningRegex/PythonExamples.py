85011aecb97ae7cc349e3beaa00fcd77bc09f23e
Igor Cortez <igor.barbosa14@gmail.com>
2020-03-21 15:54:55 -0300

    Regex with Python

---
+++ runningRegex/PythonExamples.py
@@ -0,0 +1,12 @@
+import re
+
+text = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f'
+
+pattern9 = re.compile('9')
+
+match1 = re.search(pattern9,text)
+print ("Positions: %s, %s; value: %s." % (match1.start(),match1.end(), match1.group(0)))
+
+
+values = re.findall('[a-f]',text)
+print("Valores: %s" %values)
